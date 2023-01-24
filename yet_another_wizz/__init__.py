0361b31343039bec7dd5a599a5e55c49dd48cfd5
Jan Luca van den Busch <jlvdb@astro.rub.de>
2023-01-24 13:50:33 +0000

    make the scipy and treecorr subpackages independent imports

--- yet_another_wizz/__init__.py
+++
@@ -1,5 +0,0 @@
-from .randoms import UniformRandoms
-from .core.config import Configuration
-from .core.correlation import CorrelationFunction
-from .core.redshifts import NzEstimator
-from .yaw import autocorrelate, crosscorrelate
--- yet_another_wizz/scipy/__init__.py
+++ yet_another_wizz/scipy/__init__.py
@@ -1,1 +1,10 @@
+# gloabl imports
+from .. import core
+from ..randoms import UniformRandoms
+from ..core.config import Configuration
+from ..core.correlation import CorrelationFunction
+from ..core.redshifts import NzEstimator
+from ..yaw import autocorrelate, crosscorrelate
+
+# backend specific imports
 from .catalog import Catalog
