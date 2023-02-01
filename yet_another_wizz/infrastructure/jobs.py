67b97ec114b96aacc888fa3979676e0a507d7aea
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-02-01 11:42:30 +0100

    create a basic logger before running jobs

--- yet_another_wizz/infrastructure/jobs.py
+++ yet_another_wizz/infrastructure/jobs.py
@@ -1,10 +1,29 @@
 from __future__ import annotations
 
+from functools import wraps
+from typing import Callable
+
 from yet_another_wizz.core.config import Configuration
+from yet_another_wizz.logger import get_logger
 
 from yet_another_wizz.infrastructure.project import ProjectDirectory
 
 
+def logged(func: Callable) -> Callable:
+    @wraps(func)
+    def with_logging(args):
+        levels = {0: "warn", 1: "info", 2: "debug"}
+        logger = get_logger(levels[args.verbose], plain=False)
+        # TODO: add log file at args.wdir.joinpath("events.log")
+        logger.info("running job xyz")
+        try:
+            return func(args)
+        except Exception:
+            logger.exception("an unexpected error occured")
+    return with_logging
+
+
+@logged
 def init(args):
     # parse the configuration
     config = Configuration.create(
@@ -24,5 +43,31 @@ def init(args):
         root.setup.add_catalog("ref_rand", input_rand)
 
 
-def crosscorr(args):
-    pass
+@logged
+def cross(args):
+    project = ProjectDirectory(args.wdir)
+    raise NotImplementedError
+
+
+@logged
+def auto(args):
+    project = ProjectDirectory(args.wdir)
+    raise NotImplementedError
+
+
+@logged
+def merge(args):
+    print("a")
+    raise NotImplementedError
+
+
+@logged
+def nz(args):
+    project = ProjectDirectory(args.wdir)
+    raise NotImplementedError
+
+
+@logged
+def run(args):
+    project = ProjectDirectory(args.wdir)
+    raise NotImplementedError
