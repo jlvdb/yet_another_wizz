85dc48517973628d711fba493d659b83ef3de55c
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-02-19 02:24:18 +0100

    cleand up logging output to stdout

--- yaw/logger.py
+++ yaw/logger.py
@@ -12,6 +12,28 @@ class OnlyYAWFilter(logging.Filter):
         return "yaw" in record.name
 
 
+class CustomFormatter(logging.Formatter):
+
+    sep = "|"
+    faint = "\033[2m"
+    bold = "\033[1m"
+    boldyellow = "\033[1;33m"
+    boldred = "\033[1;31m"
+    reset = "\033[0m"
+
+    FORMATS = {
+        logging.DEBUG: f"{faint}%(levelname).3s {sep} %(message)s{reset}",
+        logging.INFO: f"%(levelname).3s {sep} %(message)s",
+        logging.WARNING: f"{boldyellow}%(levelname).3s {sep}{reset} %(message)s",
+        logging.ERROR: f"{boldred}%(levelname).3s {sep} %(message)s{reset}",
+        logging.CRITICAL: f"{boldred}%(levelname).3s {sep} %(message)s{reset}"}
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno, self.FORMATS[logging.INFO])
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 def get_logger(
     level: str = "info",
     plain: bool = True
@@ -19,10 +41,10 @@ def get_logger(
     level = getattr(logging, level.upper())
     handler = logging.StreamHandler(sys.stdout)
     if plain:
-        format = "%(message)s"
+        handler.setFormatter(CustomFormatter())
     else:
         format = "%(levelname)s:%(name)s:%(message)s"
-    handler.setFormatter(logging.Formatter(format))
+        handler.setFormatter(logging.Formatter(format))
     handler.setLevel(level)
     handler.addFilter(OnlyYAWFilter())
     logging.basicConfig(level=level, handlers=[handler])
