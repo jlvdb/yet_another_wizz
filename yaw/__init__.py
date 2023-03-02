0d79e9451e4529cf57c92a92b018db5f5465b037
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-03-02 19:06:16 +0100

    cleaned up root __init__

--- yaw/__init__.py
+++ yaw/__init__.py
@@ -7,7 +7,8 @@ _logging.getLogger(__name__).addHandler(_logging.NullHandler())
 
 from yaw.catalogs import NewCatalog
 from yaw.config import Configuration, ResamplingConfig
-from yaw.correlation import CorrelationData, CorrelationFunction, RedshiftData, autocorrelate, crosscorrelate
-from yaw.cosmology import CustomCosmology
+from yaw.correlation import (
+    CorrelationData, CorrelationFunction, RedshiftData,
+    autocorrelate, crosscorrelate)
 from yaw.logger import get_logger
 from yaw.randoms import UniformRandoms
