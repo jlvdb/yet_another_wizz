fd6b7c246ae90793bbbc61b4c528410ad6c2432d
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-06-29 16:27:39 +0200

    added doc stubs for yaw.pipeline and yaw.commandline

--- yaw/commandline/__init__.py
+++ yaw/commandline/__init__.py
@@ -1,2 +1,7 @@
+"""This module implements the commandline interface for the `yaw` executable.
+Coverage of the implementation details has low priority and may be added at a
+later time.
+"""
+
 from yaw.commandline.main import Commandline
 from yaw.commandline import subcommands  # register subcommands
--- yaw/pipeline/__init__.py
+++ yaw/pipeline/__init__.py
@@ -1,3 +1,9 @@
+"""This module implements the data processing engine used by the high-level
+:mod:`yaw.commandline` package, which implements the `yaw` executable script.
+Coverage of the implementation details has low priority and may be added at a
+later time.
+"""
+
 from yaw.pipeline import tasks
 from yaw.pipeline.plot import Plotter
 from yaw.pipeline.project import ProjectDirectory
