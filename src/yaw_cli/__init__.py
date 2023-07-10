ffff60fd39419b45b051321b56a265f53af4c494
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-07-10 12:17:18 +0200

    removed the client code

--- src/yaw_cli/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This module implements the yet_another_wizz (yaw) commandline interface.
-"""
-
-from yaw_cli.commandline import Commandline
-from yaw_cli.pipeline.project import ProjectDirectory
-
-__all__ = ["Commandline", "ProjectDirectory"]
-__version__ = "1.2"
--- src/yaw_cli/commandline/__init__.py
+++
@@ -1,9 +0,0 @@
-"""This module implements the commandline interface for the `yaw` executable.
-Coverage of the implementation details has low priority and may be added at a
-later time.
-"""
-
-from yaw_cli.commandline import subcommands  # register subcommands
-from yaw_cli.commandline.main import Commandline
-
-__all__ = ["subcommands", "Commandline"]
