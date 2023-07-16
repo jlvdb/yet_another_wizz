188727cff45a524ebd9454033829d55999b6b8e5
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-07-16 19:38:02 +0200

    added a large fraction of the unittests for yaw.correlation.corrfuncs

--- tests/yaw/correlation/test_corrfunc.py
+++ tests/yaw/correlation/test_corrfunc.py
@@ -1,9 +1,10 @@
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
-from pytest import fixture, mark
+from pytest import fixture, mark, raises
 
-from yaw.correlation import corrfuncs
+from yaw import examples
+from yaw.correlation import corrfuncs, estimators
 
 
 @fixture
@@ -48,6 +49,14 @@ def correlation_data(test_data_samples):
     return corrfuncs.CorrData(binning, data, samples, method="jackknife", info="info")
 
 
+def text_grep(path, pattern, sub):
+    with open(path) as f:
+        lines = [line.replace(pattern, sub) for line in f.readlines()]
+    with open(path, "w") as f:
+        for line in lines:
+            f.write(line)
+
+
 class TestCorrData:
     def test_stats(self, test_data_samples, stats_jackknife, stats_bootstrap):
         binning, data, samples = test_data_samples
@@ -71,6 +80,14 @@ class TestCorrData:
         npt.assert_allclose(correlation_data.data, restored.data)
         npt.assert_allclose(correlation_data.samples, restored.samples)
         assert correlation_data.info == restored.info
+        # break the data file
+        text_grep(f"{path}.smp", correlation_data.method[:4], "udef")
+        with raises(ValueError):
+            correlation_data.from_files(path)
+        text_grep(f"{path}.smp", "udef", correlation_data.method[:4])
+        text_grep(f"{path}.smp", "z_low", "wrong_key")
+        with raises(ValueError):
+            correlation_data.from_files(path)
 
     @mark.parametrize("flags", [False, True])
     def test_plot(self, correlation_data, flags):
@@ -81,79 +98,212 @@ class TestCorrData:
         correlation_data.plot_corr(redshift=redshift)
 
 
-@mark.skip
 def test_check_mergable():
-    assert 0
+    corrfuncs.check_mergable([examples.w_sp, examples.w_sp])
+    # different pair counts
+    with raises(ValueError, match="incompatible"):
+        corrfuncs.check_mergable([examples.w_sp, examples.w_ss])
 
 
 class TestCorrFunc:
-    @mark.skip
     def test_init(self):
-        assert 0
+        pairs = dict(
+            dd=examples.w_ss.dd,
+            dr=examples.w_ss.dr,
+            rd=examples.w_ss.dr,  # duplicated
+            rr=examples.w_ss.rr,
+        )
+        assert examples.w_ss == corrfuncs.CorrFunc(
+            dd=pairs["dd"], dr=pairs["dr"], rr=pairs["rr"]
+        )
+        with raises(ValueError, match="required"):
+            corrfuncs.CorrFunc(dd=pairs["dd"])
+        for name, pair in pairs.items():
+            if name == "dd":
+                continue
+            corrfuncs.CorrFunc(dd=pairs["dd"], **{name: pair})
+        with raises(ValueError):
+            corrfuncs.CorrFunc(dd=pairs["dd"], dr=pairs["dr"].bins[1:])
+        with raises(ValueError):
+            corrfuncs.CorrFunc(dd=pairs["dd"], dr=pairs["dr"].patches[1:])
 
-    @mark.skip
     def test_repr(self):
-        assert 0
+        repr(examples.w_ss)
 
-    @mark.skip
     def test_eq(self):
-        assert 0
+        assert examples.w_sp == examples.w_sp
+        assert examples.w_sp != examples.w_ss
+        assert examples.w_sp != 1
 
-    @mark.skip
     def test_add(self):
-        assert 0
+        summed = examples.w_ss + examples.w_ss
+        assert summed.dd == examples.w_ss.dd + examples.w_ss.dd
+        assert summed.dr == examples.w_ss.dr + examples.w_ss.dr
+        assert summed.rr == examples.w_ss.rr + examples.w_ss.rr
+        with raises(ValueError, match="not set"):
+            new = corrfuncs.CorrFunc(dd=summed.dd, dr=summed.dr)
+            summed + new
+        with raises(TypeError):
+            summed + 1
 
-    @mark.skip
     def test_radd(self):
-        assert 0
+        assert (0 + examples.w_sp) == examples.w_sp
+        assert sum([examples.w_sp]) == examples.w_sp
+        with raises(TypeError):
+            1 + examples.w_sp
 
-    @mark.skip
     def test_mul(self):
-        assert 0
+        mul = examples.w_sp * 2
+        assert mul == examples.w_sp + examples.w_sp
+        assert mul.dd.total == examples.w_sp.dd.total
+        assert mul.dd.count == examples.w_sp.dd.count * 2
+        with raises(TypeError):
+            examples.w_sp * True
 
-    @mark.skip
     def test_auto(self):
-        assert 0
+        assert examples.w_sp.auto == examples.w_sp.dd.count.auto
 
-    @mark.skip
-    def test_bins(self):
-        assert 0
-
-    @mark.skip
-    def test_patches(self):
-        assert 0
-
-    @mark.skip
-    def test_get_binning(self):
-        assert 0
-
-    @mark.skip
     def test_n_patches(self):
-        assert 0
+        assert examples.w_sp.n_patches == examples.w_sp.dd.count.n_patches
 
-    @mark.skip
+    @mark.parametrize("items", [1, 2, slice(0, 2), [1, 2]])
+    def test_bins(self, items):
+        subset = examples.w_sp.bins[items]
+        assert subset == corrfuncs.CorrFunc(
+            dd=examples.w_sp.dd.bins[items], dr=examples.w_sp.dr.bins[items]
+        )
+
+    @mark.parametrize("items", [1, 2, slice(0, 2), [1, 2]])
+    def test_patches(self, items):
+        subset = examples.w_sp.patches[items]
+        assert subset == corrfuncs.CorrFunc(
+            dd=examples.w_sp.dd.patches[items], dr=examples.w_sp.dr.patches[items]
+        )
+
+    def test_get_binning(self):
+        assert (examples.w_sp.get_binning() == examples.w_sp.dd.get_binning()).all()
+
     def test_is_compatible(self):
-        assert 0
+        examples.w_sp.is_compatible(examples.w_sp)
+        with raises(ValueError):
+            assert not examples.w_sp.is_compatible(examples.w_sp.bins[1:])
+            assert examples.w_sp.is_compatible(examples.w_sp.bins[1:], require=True)
+        with raises(ValueError):
+            assert not examples.w_sp.is_compatible(examples.w_sp.patches[1:])
+            assert examples.w_sp.is_compatible(examples.w_sp.patches[1:], require=True)
 
-    @mark.skip
     def test_estimators(self):
-        assert 0
+        pairs = dict(
+            dd=examples.w_ss.dd,
+            dr=examples.w_ss.dr,
+            rd=examples.w_ss.dr,  # duplicated
+            rr=examples.w_ss.rr,
+        )
+        cf = corrfuncs.CorrFunc(**pairs).estimators
+        assert set(cf.keys()) == {"HM", "LS", "DP", "PH"}
+        cf = corrfuncs.CorrFunc(
+            dd=pairs["dd"], dr=pairs["dr"], rr=pairs["rr"]
+        ).estimators
+        assert set(cf.keys()) == {"HM", "LS", "DP", "PH"}
+        cf = corrfuncs.CorrFunc(
+            dd=pairs["dd"], rd=pairs["rd"], rr=pairs["rr"]
+        ).estimators
+        assert set(cf.keys()) == {"DP", "PH"}
 
-    @mark.skip
+        cf = corrfuncs.CorrFunc(
+            dd=pairs["dd"], dr=pairs["dr"], rd=pairs["rd"]
+        ).estimators
+        assert set(cf.keys()) == {"DP"}
+        cf = corrfuncs.CorrFunc(dd=pairs["dd"], rd=pairs["rd"]).estimators
+        assert set(cf.keys()) == {"DP"}
+        cf = corrfuncs.CorrFunc(dd=pairs["dd"], dr=pairs["dr"]).estimators
+        assert set(cf.keys()) == {"DP"}
+
+        cf = corrfuncs.CorrFunc(dd=pairs["dd"], rr=pairs["rr"]).estimators
+        assert set(cf.keys()) == {"PH"}
+
     def test__check_and_select_estimator(self):
-        assert 0
+        pairs = dict(
+            dd=examples.w_ss.dd,
+            dr=examples.w_ss.dr,
+            rd=examples.w_ss.dr,  # duplicated
+            rr=examples.w_ss.rr,
+        )
 
-    @mark.skip
+        cf = corrfuncs.CorrFunc(**pairs)
+        with raises(ValueError, match="invalid"):
+            cf._check_and_select_estimator("undefined")
+        assert cf._check_and_select_estimator() == estimators.LandySzalay
+        for est in (
+            estimators.LandySzalay,
+            estimators.Hamilton,
+            estimators.DavisPeebles,
+            estimators.PeeblesHauser,
+        ):
+            assert cf._check_and_select_estimator(est.short) == est
+
+        for pair in ("dr", "rd"):
+            cf = corrfuncs.CorrFunc(dd=pairs["dd"], **{pair: pairs[pair]})
+            assert cf._check_and_select_estimator() == estimators.DavisPeebles
+            for est in (estimators.DavisPeebles,):
+                assert cf._check_and_select_estimator(est.short) == est
+            for est in (
+                estimators.LandySzalay,
+                estimators.Hamilton,
+                estimators.PeeblesHauser,
+            ):
+                with raises(estimators.EstimatorError, match="requires"):
+                    cf._check_and_select_estimator(est.short)
+
+        cf = corrfuncs.CorrFunc(dd=pairs["dd"], rr=pairs["rr"])
+        assert cf._check_and_select_estimator() == estimators.PeeblesHauser
+        for est in (estimators.PeeblesHauser,):
+            assert cf._check_and_select_estimator(est.short) == est
+        for est in (
+            estimators.LandySzalay,
+            estimators.Hamilton,
+            estimators.DavisPeebles,
+        ):
+            with raises(estimators.EstimatorError, match="requires"):
+                cf._check_and_select_estimator(est.short)
+
     def test__getattr_from_cts(self):
-        assert 0
+        pairs = dict(
+            dd=examples.w_ss.dd,
+            dr=examples.w_ss.dr,
+            rd=examples.w_ss.dr,  # duplicated
+            rr=examples.w_ss.rr,
+        )
+        with_rd = corrfuncs.CorrFunc(**pairs)
+        rd = pairs.pop("rd")
+        without_rd = corrfuncs.CorrFunc(**pairs)
+        # CtsDD
+        assert with_rd.dd == with_rd._getattr_from_cts(estimators.CtsDD())
+        assert with_rd.dd == without_rd._getattr_from_cts(estimators.CtsDD())
+        # CtsDR
+        assert with_rd.dr == with_rd._getattr_from_cts(estimators.CtsDR())
+        assert with_rd.dr == without_rd._getattr_from_cts(estimators.CtsDR())
+        # CtsRD
+        assert with_rd.rd == with_rd._getattr_from_cts(estimators.CtsRD())
+        assert without_rd._getattr_from_cts(estimators.CtsRD()) is None
+        # CtsRR
+        assert with_rd.rr == with_rd._getattr_from_cts(estimators.CtsRR())
+        assert with_rd.rr == without_rd._getattr_from_cts(estimators.CtsRR())
+        # CtsMix
+        assert with_rd.dr == with_rd._getattr_from_cts(estimators.CtsMix())
+        assert with_rd.dr == without_rd._getattr_from_cts(estimators.CtsMix())
+        without_dr = corrfuncs.CorrFunc(**pairs, rd=rd)
+        assert without_dr.rd == without_dr._getattr_from_cts(estimators.CtsMix())
 
     @mark.skip
     def test_sample(self):
         assert 0
 
-    @mark.skip
-    def test_io(self):
-        assert 0
+    @mark.parametrize("cf", [examples.w_sp, examples.w_ss, examples.w_pp])
+    def test_io(self, tmp_path_factory, cf):
+        path = tmp_path_factory.mktemp("corr_func_file") / "testfile.hdf"
+        cf.to_file(path)
+        assert corrfuncs.CorrFunc.from_file(path) == cf
 
     @mark.skip
     def test_concatenate_patches(self):
