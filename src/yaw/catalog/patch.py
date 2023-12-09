fa20287e8e75345d5736e85505d61c62a2e4fd29
Jan Luca van den Busch <jlvdb@astro.rub.de>
2023-12-09 14:19:06 +0100

    cleaned up patch submodule

--- src/yaw/catalog/patch.py
+++ src/yaw/catalog/patch.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 from copy import copy
 from dataclasses import asdict, dataclass, field
 from pathlib import Path
-from typing import TYPE_CHECKING, Generator, overload
+from typing import TYPE_CHECKING, Generator, TypeVar, overload
 
 import numpy as np
 
@@ -34,18 +34,45 @@ __all__ = [
 ]
 
 
-@dataclass
+T = TypeVar("T")
+
+
+@overload
+def ensure_notset_is_none(obj: None) -> None:
+    ...
+
+
+@overload
+def ensure_notset_is_none(obj: NotSet) -> None:
+    ...
+
+
+@overload
+def ensure_notset_is_none(obj: T) -> T:
+    ...
+
+
+@overload
+def ensure_notset_is_none(obj: T | NotSet | None) -> T | None:
+    return None if obj is None or obj is NotSet else obj
+
+
+@dataclass(frozen=True)
 class PatchMetadata:
     length: int
     total: float | NotSet = field(default=NotSet)
     center: CoordSky | NotSet = field(default=NotSet)
     radius: DistSky | NotSet = field(default=NotSet)
 
+    def set_center(self, center: Coordinate):
+        object.__setattr__(self, "center", center.to_sky())
+
     def compute_total(self, weight: NDArray | None) -> None:
         if weight is None:
-            self.total = float(self.length)
+            total = float(self.length)
         else:
-            self.total = float(weight.sum())
+            total = float(weight.sum())
+        object.__setattr__(self, "total", total)
 
     def compute_center_radius(
         self,
@@ -55,14 +82,10 @@ class PatchMetadata:
         radius: Distance | None = None,
     ) -> None:
         coords = CoordSky(ra, dec)
-        if center is None:
-            self.center = coords.mean()
-        else:
-            self.center = center.to_sky()
-        if radius is None:
-            self.radius = coords.max_dist(self.center)
-        else:
-            self.radius = radius.to_sky()
+        center = coords.mean() if center is None else center.to_sky()
+        radius = coords.max_dist(center) if radius is None else radius.to_sky()
+        object.__setattr__(self, "center", center)
+        object.__setattr__(self, "radius", radius)
 
     @classmethod
     def new_with_length(self, length: int) -> PatchMetadata:
@@ -77,34 +100,36 @@ class PatchMetadata:
         metadict = {
             attr: value for attr, value in asdict(self).items() if value is not NotSet
         }
-        # replace center with ra/dec floats
+
         try:
             center: CoordSky = metadict.pop("center")
             metadict["ra"] = float(center.ra)
             metadict["dec"] = float(center.dec)
         except KeyError:
             pass
-        # convert radius to float
+
         try:
             radius: DistSky = metadict.pop("radius")
             metadict["radius"] = float(radius.values)
         except KeyError:
             pass
+
         return metadict
 
     @classmethod
     def from_dict(cls, the_dict: dict) -> PatchMetadata:
         kwargs = {k: v for k, v in the_dict.items()}
-        # reconstruct center
+
         try:
             kwargs["center"] = CoordSky(kwargs.pop("ra"), kwargs.pop("dec"))
         except KeyError:
             pass
-        # reconstruct radius
+
         try:
             kwargs["radius"] = DistSky(kwargs["radius"])
         except KeyError:
             pass
+
         return cls(**kwargs)
 
 
@@ -184,9 +209,7 @@ class PatchData:
             :obj:`yaw.core.coordinates.DistSky`
         """
         if self.metadata.radius is NotSet:
-            center = self.metadata.center
-            if center is NotSet:
-                center = None
+            center = ensure_notset_is_none(self.metadata.center)
             self.metadata.compute_center_radius(self.ra, self.dec, center)
             self._update_metadata_callback()
         return self.metadata.radius
@@ -194,15 +217,15 @@ class PatchData:
     def iter_bins(
         self,
         z_bins: NDArray[np.float64],
-        allow_no_redshift: bool = False,
+        require_redshift: bool = True,
     ) -> Generator[tuple[Interval, PatchData]]:
         """Iterate the patch in bins of redshift.
 
         Args:
             z_bins (:obj:`NDArray`):
                 Edges of the redshift bins.
-            allow_no_redshift (:obj:`bool`):
-                If true and the data has no redshifts, the iterator yields the
+            require_redshift (:obj:`bool`):
+                If false and the data has no redshifts, the iterator yields the
                 whole patch at each iteration step.
 
         Yields:
@@ -211,48 +234,53 @@ class PatchData:
                 - **cat** (:obj:`PatchCatalog`): instance containing the data
                   for this bin.
         """
-        z_bins = np.asarray(z_bins)
-        if not allow_no_redshift and not self.has_redshift:
+        if require_redshift and not self.has_redshift:
             raise ValueError("no redshifts for iteration provdided")
-        intervals = Binning.from_edges(z_bins, closed="left")
-        if allow_no_redshift:
-            for intv in intervals:
-                yield intv, self
-        else:
-            redshift = self.redshift
-            bin_index = intervals.apply(redshift)
+        intervals = Binning.from_edges(np.asarray(z_bins), closed="left")
+        n_bins = len(intervals)
+
+        if require_redshift:
+            bin_index = intervals.apply(self.redshift)
             index_to_interval = dict(enumerate(intervals))
             data = utils.DataChunk(
                 ra=self.ra,
                 dec=self.dec,
                 weight=self.weight,
-                redshift=redshift,
-                patch=bin_index,  # this is currently required by the implementation
+                redshift=self.redshift,
+                patch=bin_index,  # this is currently required by .groupby()
             )
             for index, bin_data in data.groupby():
-                if index < 0 or index >= len(intervals):
-                    continue
-                intv = index_to_interval[index]
-                yield intv, PatchData(
-                    self.id,
-                    metadata=self.metadata.new_with_length(len(bin_data)),
-                    **bin_data.to_dict(),
-                )
+                if 0 <= index < n_bins:
+                    metadata = self.metadata.new_with_length(len(bin_data))
+                    kwargs = bin_data.to_dict()
+                    patch_bin = PatchData(self.id, metadata=metadata, **kwargs)
+                    yield index_to_interval[index], patch_bin
+
+        else:
+            for intv in intervals:
+                yield intv, self
 
 
 class PatchDataResident(PatchData):
-    pass
+    pass  # alias to express data layout through type instead of state
 
 
-def load_attribute(
-    path: TypePathStr, which: str, require: bool = True
-) -> np.memmap | None:
-    mempath = path / which
-    try:
-        return utils.memmap_load(mempath, np.float64)
-    except FileNotFoundError as err:
-        if require:
-            raise FileNotFoundError(f"missing '{which}' data: {mempath}") from err
+def get_and_check_data_size(
+    path: TypePathStr,
+    *,
+    must_exist: bool,
+    require_size: int | None = None,
+    itemsize: int = 8,
+) -> int | None:
+    if path.exists():
+        nbytes = os.path.getsize(path)
+        size = nbytes // itemsize
+        if size != require_size:
+            raise ValueError("file size does not match expected size")
+        return size
+
+    elif must_exist:
+        raise FileNotFoundError(str(path))
 
 
 class PatchDataCached(PatchData):
@@ -275,54 +303,7 @@ class PatchDataCached(PatchData):
         writer.finalize()
         self.path = writer.path
         # populate the data attributes
-        self._init_data(metadata)
-
-    @property
-    def _path_metadata(self) -> Path:
-        return self.path / "metadata.json"
-
-    def _write_metadata(self) -> None:
-        with open(self._path_metadata, "w") as f:
-            the_dict = self.metadata.to_dict()
-            json.dump(the_dict, f)
-
-    def _update_metadata_callback(self) -> None:
-        self._write_metadata()
-
-    def _check_attr_size(
-        self, attr: str, must_exist: bool, size: int | None = None
-    ) -> int:
-        try:
-            fsize = os.path.getsize(self.path / attr) // 8  # is 8 byte double
-        except FileNotFoundError:
-            if must_exist:
-                raise
-        if size is None:
-            return fsize
-        elif fsize != size:
-            raise ValueError(
-                f"'{attr}' size ({fsize}) does not match expected size ({size})"
-            )
-        return size
-
-    def _init_data(self, metadata: PatchMetadata | None) -> None:
-        # check that ra and dec exist
-        length = self._check_attr_size("ra", must_exist=True)
-        self._check_attr_size("dec", must_exist=True, size=length)
-        # check the optional data
-        self._check_attr_size("weight", must_exist=False, size=length)
-        self._check_attr_size("redshift", must_exist=False, size=length)
-        # load the metadata
-        if metadata is not None:
-            self.metadata = metadata
-            self._write_metadata()
-        elif self._path_metadata.exists():
-            with open(self._path_metadata) as f:
-                the_dict = json.load(f)
-            self.metadata = PatchMetadata.from_dict(the_dict)
-        else:
-            self.metadata = PatchMetadata(length)
-            self._write_metadata()
+        self._init_from_disk(metadata)
 
     @classmethod
     def restore(cls, id: int, path: TypePathStr) -> PatchDataCached:
@@ -332,8 +313,49 @@ class PatchDataCached(PatchData):
             raise FileNotFoundError(f"cache directory des not exist: {new.path}")
         new.id = id
         # populate the data attributes
-        new._init_data(None)
+        new._init_from_disk(metadata=None)
         return new
+
+    @property
+    def _path_metadata(self) -> Path:
+        return self.path / "metadata.json"
+
+    def _write_metadata(self) -> None:
+        the_dict = self.metadata.to_dict()
+        with open(self._path_metadata, "w") as f:
+            json.dump(the_dict, f)
+
+    _update_metadata_callback = _write_metadata
+
+    def _check_data_files(self) -> None:
+        # required data
+        length = get_and_check_data_size("ra", must_exist=True)
+        get_and_check_data_size("dec", must_exist=True, require_size=length)
+        # optional data
+        get_and_check_data_size("weight", must_exist=False, require_size=length)
+        get_and_check_data_size("redshift", must_exist=False, require_size=length)
+        return length
+
+    def _init_metadata(self, data_length: int, metadata: PatchMetadata | None) -> None:
+        overwrite = metadata is not None
+        load_existing = self._path_metadata.exists()
+
+        if overwrite:
+            self.metadata = metadata
+            self._write_metadata()
+
+        elif load_existing:
+            with open(self._path_metadata) as f:
+                the_dict = json.load(f)
+            self.metadata = PatchMetadata.from_dict(the_dict)
+
+        else:
+            self.metadata = PatchMetadata(data_length)
+            self._write_metadata()
+
+    def _init_from_disk(self, metadata: PatchMetadata | None) -> None:
+        length = self._check_data_files()
+        self._init_metadata(length, metadata)
 
     @property
     def ra(self) -> NDArray[np.float64]:
@@ -358,25 +380,22 @@ class PatchDataCached(PatchData):
             return None
 
 
+@dataclass
 class CacheWriter:
-    def __init__(
-        self,
-        path: Path,
-        has_weight: bool = False,
-        has_redshift: bool = False,
-        chunksize: int = 65_536,
-    ) -> None:
-        self.cache = {"ra": utils.ArrayCache(), "dec": utils.ArrayCache()}
-        self.has_weight = has_weight
-        if has_weight:
-            self.cache["weight"] = utils.ArrayCache()
-        self.has_redshift = has_redshift
-        if has_redshift:
-            self.cache["redshift"] = utils.ArrayCache()
+    path: TypePathStr
+    has_weight: bool = field(default=False)
+    has_redshift: bool = field(default=False)
+    chunksize: int = field(default=65_536)
+
+    def __post_init__(self) -> None:
         self.cachesize = 0
-        self.chunksize = chunksize
-        # create the cache directory
-        self.path = Path(path)
+        self.cache = {"ra": utils.ArrayCache(), "dec": utils.ArrayCache()}
+        if self.has_weight:
+            self.cache["weight"] = utils.ArrayCache()
+        if self.has_redshift:
+            self.cache["redshift"] = utils.ArrayCache()
+
+        self.path = Path(self.path)
         if self.path.exists():
             raise FileExistsError(f"directory already exists: {self.path}")
         self.path.mkdir(parents=True)
@@ -388,24 +407,21 @@ class CacheWriter:
             cache.clear()
         self.cachesize = 0
 
+    def _check_data_schema(data: dict, name: str, is_expected: bool) -> None:
+        is_provided = name in data
+        if is_provided and not is_expected:
+            raise KeyError(f"'{name}' is provided but not expected")
+        elif not is_provided and is_expected:
+            raise KeyError(f"'{name}' is not provided but expected")
+
     def append_chunk(self, chunk: DataChunk) -> None:
-        # check the data inputs
         chunk_dict = chunk.to_dict(drop_patch=True)
-        has_weight = "weight" in chunk_dict
-        if has_weight != self.has_weight:
-            raise ValueError(
-                f"writer has_weights={self.has_weight}, but chunk {has_weight=}"
-            )
-        has_redshift = "redshift" in chunk_dict
-        if has_redshift != self.has_redshift:
-            raise ValueError(
-                f"writer has_weights={self.has_redshift}, but chunk {has_redshift=}"
-            )
-        # send data to cache
+        self._check_data_schema(chunk_dict, "weight", is_expected=self.has_weight)
+        self._check_data_schema(chunk_dict, "redshift", is_expected=self.has_redshift)
+
         self.cachesize += len(chunk)
         for key, cache in self.cache.items():
             cache.append(chunk_dict[key])
-        # flush to disk
         if self.cachesize > self.chunksize:
             self.flush()
 
@@ -422,7 +438,76 @@ class CacheWriter:
         self.flush()
 
 
-# the constructor functions
+class Collector(ABC):
+    @abstractmethod
+    def process(
+        self,
+        chunk: DataChunk,
+        patch_key: str,
+        drop_key: bool = True,
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def get_patches(self) -> dict[int, PatchData]:
+        pass
+
+
+class PatchCollector(Collector):
+    def __init__(self) -> None:
+        self._chunks: dict[int, list[DataChunk]] = defaultdict(list)
+
+    def process(self, chunk: DataChunk) -> None:
+        for patch_id, patch_chunk in chunk.groupby():
+            self._chunks[patch_id].append(patch_chunk)
+
+    def close(self) -> None:
+        pass
+
+    def get_patches(self) -> dict[int, PatchDataResident]:
+        concatenated = {
+            patch_id: DataChunk.from_chunks(chunks)
+            for patch_id, chunks in self._chunks.items()
+        }
+        return {
+            patch_id: PatchDataResident(patch_id, **chunk.to_dict())
+            for patch_id, chunk in concatenated.items()
+        }
+
+
+class PatchWriter(Collector):
+    def __init__(self, cache_directory: TypePathStr) -> None:
+        self._writers: dict[int, CacheWriter] = dict()
+
+        self.cache_directory = Path(cache_directory)
+        self.cache_directory.mkdir(parents=True, exist_ok=True)
+
+    def _get_and_delete_cache_path(self, patch_id: int) -> Path:
+        cachepath = patch_path_from_id(self.cache_directory, patch_id)
+        if os.path.exists(cachepath):
+            shutil.rmtree(cachepath)
+
+    def process(self, chunk: DataChunk) -> None:
+        for patch_id, patch_chunk in chunk.groupby():
+            if patch_id not in self._writers:
+                cachepath = self._get_and_delete_cache_path(patch_id)
+                self._writers[patch_id] = CacheWriter(
+                    cachepath,
+                    has_weight=patch_chunk.weight is not None,
+                    has_redshift=patch_chunk.redshift is not None,
+                )
+            self._writers[patch_id].append_chunk(patch_chunk)
+
+    def get_patches(self) -> dict[int, PatchDataCached]:
+        for writer in self._writers.values():
+            writer.finalize()
+        return {
+            patch_id: PatchDataCached.restore(patch_id, writer.path)
+            for patch_id, writer in self._writers.items()
+        }
+
+
+# the Patch constructor functions
 
 
 @overload
@@ -486,68 +571,3 @@ def patch_from_records(
             redshift=redshift,
             metadata=metadata,
         )
-
-
-class Collector(ABC):
-    @abstractmethod
-    def process(
-        self,
-        chunk: DataChunk,
-        patch_key: str,
-        drop_key: bool = True,
-    ) -> None:
-        pass
-
-    @abstractmethod
-    def get_patches(self) -> dict[int, PatchData]:
-        pass
-
-
-class PatchCollector(Collector):
-    def __init__(self) -> None:
-        self._chunks: dict[int, list[DataChunk]] = defaultdict(list)
-
-    def process(self, chunk: DataChunk) -> None:
-        for pid, patch_chunk in chunk.groupby():
-            self._chunks[pid].append(patch_chunk)
-
-    def close(self) -> None:
-        pass
-
-    def get_patches(self) -> dict[int, PatchDataResident]:
-        data = {
-            pid: DataChunk.from_chunks(chunks) for pid, chunks in self._chunks.items()
-        }
-        return {
-            pid: PatchDataResident(pid, **data.to_dict()) for pid, data in data.items()
-        }
-
-
-class PatchWriter(Collector):
-    def __init__(self, cache_directory: TypePathStr) -> None:
-        self._writers: dict[int, CacheWriter] = dict()
-        # set up cache directory
-        self.cache_directory = Path(cache_directory)
-        if not self.cache_directory.exists():
-            self.cache_directory.mkdir(parents=True)
-
-    def process(self, chunk: DataChunk) -> None:
-        for pid, patch_chunk in chunk.groupby():
-            if pid not in self._writers:
-                cachepath = patch_path_from_id(self.cache_directory, pid)
-                if os.path.exists(cachepath):
-                    shutil.rmtree(cachepath)
-                self._writers[pid] = CacheWriter(
-                    cachepath,
-                    has_weight=patch_chunk.weight is not None,
-                    has_redshift=patch_chunk.redshift is not None,
-                )
-            self._writers[pid].append_chunk(patch_chunk)
-
-    def get_patches(self) -> dict[int, PatchDataCached]:
-        for writer in self._writers.values():
-            writer.finalize()
-        return {
-            pid: PatchDataCached.restore(pid, writer.path)
-            for pid, writer in self._writers.items()
-        }
