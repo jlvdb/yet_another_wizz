dd8b574e54a803f7716428cd7df47d27a3eb923c
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-08-05 12:27:38 +0000

    drafted measurement functions to return original CorrFunc

--- src/yaw/measurements.py
+++ src/yaw/measurements.py
@@ -1,10 +1,15 @@
+# TODO: multiple scales
+
 from __future__ import annotations
 
+from dataclasses import dataclass
 from itertools import compress
 from collections.abc import Iterator
 from copy import deepcopy
 
+import pandas as pd  # TODO: remove dependecy?
 import numpy as np
+from numpy.typing import NDArray
 
 from yaw.catalog import Catalog, Patch
 from yaw.catalog.catalog import InconsistentPatchesError
@@ -12,7 +17,26 @@ from yaw.config import Configuration
 from yaw.coordinates import DistsSky
 from yaw.core.cosmology import r_kpc_to_angle
 from yaw.correlation import CorrFunc
+from yaw.correlation.paircounts import NormalisedCounts, PatchedCount, PatchedTotal
 from yaw.parallel import ParallelHelper
+
+
+@dataclass(frozen=True)
+class PatchPair:
+    id1: int
+    id2: int
+    patch1: Patch
+    patch2: Patch
+
+
+@dataclass(frozen=True)
+class PatchPaircounts:
+    """TODO: delete original version in yaw.core.containers"""
+    id1: int
+    id2: int
+    totals1: NDArray
+    totals2: NDArray
+    counts: NDArray
 
 
 def check_patch_conistency(catalog: Catalog, *catalogs: Catalog, rtol: float = 0.5):
@@ -108,22 +132,27 @@ class PatchLinkage:
         self,
         catalog1: Catalog,
         catalog2: Catalog | None = None,
-    ) -> tuple[tuple[Patch, Patch]]:
+    ) -> tuple[PatchPair]:
         auto = catalog2 is None
         if auto:
             catalog2 = catalog1
         return tuple(
-            (catalog1[patch_id1], catalog2[patch_id2])
+            PatchPair(patch_id1, patch_id2, catalog1[patch_id1], catalog2[patch_id2])
             for patch_id1, patch_id2 in self.iter_patch_id_pairs(auto=auto)
         )
 
 
-def count_pairs_patches(patches: tuple[str, str], config: Configuration):
-    trees1 = patches[0].get_trees()
-    trees2 = patches[1].get_trees()
+def process_patch_pair(patch_pair: PatchPair, config: Configuration) -> PatchPaircounts:
+    trees1 = patch_pair.patch1.get_trees()
+    trees2 = patch_pair.patch2.get_trees()
 
+    totals1 = 0.0
+    totals2 = 0.0
     binned_counts = []
     for i, (tree1, tree2) in enumerate(zip(iter(trees1), iter(trees2))):
+        totals1 += tree1.total
+        totals1 += tree2.total
+
         zmid = (config.binning.zbins[i] / config.binning.zbins[i+1]) / 2.0
         counts = tree1.count(
             tree2,
@@ -133,7 +162,45 @@ def count_pairs_patches(patches: tuple[str, str], config: Configuration):
             weight_res=config.scales.rbin_num,
         )
         binned_counts.append(counts)
-    return np.transpose(binned_counts)
+    counts = np.array(binned_counts)
+
+    return PatchPaircounts(patch_pair.id1, patch_pair.id2, totals1, totals2, counts)
+
+
+def count_pairs(config: Configuration, linkage: PatchLinkage, *catalogs: Catalog, progress: bool = False) -> NormalisedCounts:
+    """TODO: derived from yaw.catalogs.scipy.utils.merge_pairs_patches"""
+    patch_pairs = linkage.get_patch_pairs(*catalogs)
+    auto = len(catalogs) == 1
+    num_patches = len(catalogs[0])
+
+    binning = pd.IntervalIndex.from_breaks(config.binning.zbins)
+    num_bins = len(binning)
+
+    totals1 = np.zeros((num_patches, num_bins))
+    totals2 = np.zeros((num_patches, num_bins))
+    patched_counts = PatchedCount.zeros(binning, num_patches, auto=auto)
+
+    for pair_counts in ParallelHelper.iter_unordered(
+        process_patch_pair,
+        patch_pairs,
+        total=len(patch_pairs),
+        func_args=(config,),
+        progress=progress,
+    ):
+        id1 = pair_counts.id1
+        id2 = pair_counts.id2
+
+        totals1[id1] = pair_counts.totals1
+        totals2[id2] = pair_counts.totals2
+
+        counts = pair_counts.counts
+        if auto and id1 == id2:
+            counts = counts * 0.5  # autocorrelation pairs are counted twice
+        # TODO: index 0 selects only the first scale
+        patched_counts.set_measurement((id1, id2), counts[:, 0])
+
+    total = PatchedTotal(binning=binning, totals1=totals1, totals2=totals2, auto=auto)
+    return NormalisedCounts(count=patched_counts, total=total)
 
 
 def autocorrelate(
@@ -143,29 +210,21 @@ def autocorrelate(
     *,
     compute_rr: bool = True,
     progress: bool = False,
-) -> CorrFunc | dict[str, CorrFunc]:
+) -> CorrFunc:
     data.build_trees(config.binning.zbins, progress=progress)
     random.build_trees(config.binning.zbins, progress=progress)
-
     linkage = PatchLinkage.from_catalogs(config, data, random)
-    kwargs = dict(func_args=(config,), progress=progress)
 
-    patches = linkage.get_patch_pairs(data)
-    dd = []
-    for result in ParallelHelper.iter_unordered(count_pairs_patches, patches, total=len(patches), **kwargs):
-        pass
+    dd = count_pairs(config, linkage, data, progress=progress)
 
-    patches = linkage.get_patch_pairs(data, random)
-    dr = []
-    for result in ParallelHelper.iter_unordered(count_pairs_patches, patches, total=len(patches), **kwargs):
-        pass
+    dr = count_pairs(config, linkage, data, random, progress=progress)
 
     if compute_rr:
-        patches = linkage.get_patch_pairs(random)
-        rr = []
-        for result in ParallelHelper.iter_unordered(count_pairs_patches, patches, total=len(patches), **kwargs):
-            pass
+        rr = count_pairs(config, linkage, random, progress=progress)
+    else:
+        rr = None
 
+    return CorrFunc(dd, dr, rr)
 
 
 def crosscorrelate(
@@ -176,5 +235,38 @@ def crosscorrelate(
     ref_rand: Catalog | None = None,
     unk_rand: Catalog | None = None,
     progress: bool = False,
-) -> CorrFunc | dict[str, CorrFunc]:
-    pass
+) -> CorrFunc:
+    have_ref_rand = ref_rand is not None
+    have_unk_rand = unk_rand is not None
+    if not have_ref_rand and not have_unk_rand:
+        raise ValueError("at least one random dataset must be provided")
+
+    reference.build_trees(config.binning.zbins, progress=progress)
+    unknown.build_trees(config.binning.zbins, progress=progress)
+    randoms = []
+    if have_ref_rand:
+        ref_rand.build_trees(config.binning.zbins, progress=progress)
+        randoms.append(ref_rand)
+    if have_unk_rand:
+        unk_rand.build_trees(config.binning.zbins, progress=progress)
+        randoms.append(unk_rand)
+    linkage = PatchLinkage.from_catalogs(config, reference, unknown, *randoms)
+
+    dd = count_pairs(config, linkage, reference, unknown, progress=progress)
+
+    if have_unk_rand:
+        dr = count_pairs(config, linkage, reference, unk_rand, progress=progress)
+    else:
+        dr = None
+
+    if have_ref_rand:
+        rd = count_pairs(config, linkage, ref_rand, unknown, progress=progress)
+    else:
+        rd = None
+
+    if have_ref_rand and have_unk_rand:
+        rr = count_pairs(config, linkage, ref_rand, unk_rand, progress=progress)
+    else:
+        rr = None
+
+    return CorrFunc(dd, dr, rd, rr)
