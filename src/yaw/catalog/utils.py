43ece2cef75ad76d76cc7c5f4e1e78f421f40217
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-08-29 11:38:16 +0000

    rewrote DataChunk to store data in single numpy array with complex dtype

--- src/yaw/catalog/utils.py
+++ src/yaw/catalog/utils.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from typing import Any, Generator, Union
 
 import numpy as np
-from numpy.typing import ArrayLike, DTypeLike, NDArray
+from numpy.typing import ArrayLike, NDArray
 
-from yaw.containers import Serialisable, Tclosed, default_closed
+from yaw.containers import Tclosed, default_closed
 from yaw.utils import AngularCoordinates
 
 __all__ = [
@@ -50,19 +50,15 @@ def groupby_binning(
             yield binning[i - 1 : i + 1], bin_array
 
 
-class DataChunk(Serialisable):
-    __slots__ = ("coords", "weights", "redshifts", "patch_ids")
+class DataChunk:
+    __slots__ = ("data", "patch_ids")
 
     def __init__(
         self,
-        coords: AngularCoordinates,
-        weights: NDArray | None = None,
-        redshifts: NDArray | None = None,
+        data: NDArray,
         patch_ids: NDArray[np.int32] | None = None,
     ) -> None:
-        self.coords = coords
-        self.weights = weights
-        self.redshifts = redshifts
+        self.data = data
         self.set_patch_ids(patch_ids)
 
     @classmethod
@@ -70,83 +66,77 @@ class DataChunk(Serialisable):
         cls,
         ra: NDArray,
         dec: NDArray,
+        *,
         weights: NDArray | None = None,
         redshifts: NDArray | None = None,
         patch_ids: NDArray | None = None,
         degrees: bool = True,
-        chkfinite: bool = False,
     ):
-        def parser(arr: NDArray | None, dtype: DTypeLike) -> NDArray | None:
-            if arr is None:
-                return None
-            if chkfinite:
-                return np.asarray_chkfinite(arr, dtype=dtype)
-            return arr.astype(dtype, casting="same_kind", copy=False)
 
-        ra = parser(ra, np.float64)
-        dec = parser(dec, np.float64)
-        coords = np.column_stack((ra, dec))
-        if degrees:
-            coords = np.deg2rad(coords)
+        dtype = [("ra", "f8"), ("dec", "f8")]
+        if weights is not None:
+            dtype.append(("weights", "f8"))
+        if redshifts is not None:
+            dtype.append(("redshifts", "f8"))
 
-        return cls(
-            coords=AngularCoordinates(coords),
-            weights=parser(weights, np.float64),
-            redshifts=parser(redshifts, np.float64),
-            patch_ids=parser(patch_ids, np.int32),
-        )
+        data = np.empty(len(ra), dtype=dtype)
+        data["ra"] = np.deg2rad(ra) if degrees else ra
+        data["dec"] = np.deg2rad(dec) if degrees else dec
+        if weights is not None:
+            data["weights"] = weights
+        if redshifts is not None:
+            data["redshifts"] = redshifts
+
+        return cls(data, patch_ids)
 
     @classmethod
     def from_chunks(cls, chunks: Sequence[DataChunk]) -> DataChunk:
-        def concat_optional_attr(attr: str) -> NDArray | None:
-            values = tuple(getattr(chunk, attr) for chunk in chunks)
-            value_is_set = tuple(value is not None for value in values)
-            if all(value_is_set):
-                return np.concatenate(values)
-            elif not any(value_is_set):
-                return None
-            raise ValueError(f"not all chunks have '{attr}' set")
+        data = np.concatenate([chunk.data for chunk in chunks])
+        if any(chunk.patch_ids is not None for chunk in chunks):
+            patch_ids = np.concatenate([chunk.patch_ids for chunk in chunks])
 
-        return DataChunk(
-            coords=AngularCoordinates.from_coords(chunk.coords for chunk in chunks),
-            weights=concat_optional_attr("weights"),
-            redshifts=concat_optional_attr("redshifts"),
-            patch_ids=concat_optional_attr("patch_ids"),
-        )
+        return cls(data, patch_ids)
 
-    def to_dict(self) -> dict[str, Any]:
-        the_dict = dict(coords=self.coords)
-        if self.weights is not None:
-            the_dict["weights"] = self.weights
-        if self.redshifts is not None:
-            the_dict["redshifts"] = self.redshifts
-        if self.patch_ids is not None:
-            the_dict["patch_ids"] = self.patch_ids
-        return the_dict
+    def view(self) -> NDArray:
+        num_cols = len(self.data.dtype.fields)
+        return self.data.view("f8").reshape(-1, num_cols)
+
+    @property
+    def coords(self) -> AngularCoordinates:
+        return AngularCoordinates(self.view()[:, :2])
+
+    @property
+    def weights(self) -> NDArray | None:
+        if "weights" in self.data.dtype.fields:
+            return self.view()[:, 2]
+        return None
+
+    @property
+    def redshifts(self) -> NDArray | None:
+        if "redshifts" in self.data.dtype.fields:
+            idx_col = 2 + ("weights" in self.data.dtype.fields)
+            return self.view()[:, idx_col]
+        return None
 
     def split(self, num_chunks: int) -> list[DataChunk]:
-        splitted = dict(
-            coords=[
-                AngularCoordinates(v) for v in np.array_split(self.coords, num_chunks)
-            ]
-        )
-        for attr, values in self.to_dict().items():
-            splits = np.array_split(values, num_chunks)
-            if attr == "coords":
-                splits = [AngularCoordinates(split) for split in splits]
-            splitted[attr] = splits
+        splits_data = np.array_split(self.data, num_chunks)
+        if self.patch_ids is not None:
+            splits_patch_ids = np.array_split(self.patch_ids, num_chunks)
+        else:
+            splits_patch_ids = [None] * num_chunks
 
         return [
-            DataChunk.from_dict({attr: values[i] for attr, values in splitted.items()})
-            for i in range(num_chunks)
+            DataChunk(data, patch_ids)
+            for data, patch_ids in zip(splits_data, splits_patch_ids)
         ]
 
     def __len__(self) -> int:
-        return len(self.coords)
+        return len(self.data)
 
     def __getitem__(self, index: ArrayLike) -> DataChunk:
-        return DataChunk.from_dict(
-            {attr: values[index] for attr, values in self.to_dict().items()}
+        return DataChunk(
+            self.data[index],
+            self.patch_ids[index] if self.patch_ids is not None else None,
         )
 
     def set_patch_ids(self, patch_ids: NDArray | None):
@@ -160,12 +150,11 @@ class DataChunk(Serialisable):
 
     def split_patches(self) -> dict[int, DataChunk]:
         if self.patch_ids is None:
-            raise ValueError("'patch_ids' not provided")
+            raise ValueError("'patch_ids' not set")
 
         chunks = {}
-        for patch_id, attr_dict in groupby_value(self.patch_ids, **self.to_dict()):
-            coords = AngularCoordinates(attr_dict.pop("coords"))
-            chunks[int(patch_id)] = DataChunk(coords, **attr_dict)
+        for patch_id, data_dict in groupby_value(self.patch_ids, data=self.data):
+            chunks[int(patch_id)] = DataChunk(**data_dict)
 
         return chunks
 
