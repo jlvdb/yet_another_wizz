bb6d3c008bd37f9fe98100f854f0b7be7bd614bc
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-09-06 19:09:27 +0000

    added a template for the new yaw commandline entrypoint

---
+++ src/yaw/cli/__main__.py
@@ -0,0 +1,5 @@
+from yaw.cli.commandline import main
+
+__all__ = [
+    "main",
+]
---
+++ src/yaw/cli/commandline.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="yaw_cli")
+    parser.parse_args()
