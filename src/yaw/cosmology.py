5bf605054ad3fc1702f1a7463a048462298ae31c
Jan Luca van den Busch <jlvandenbusch@googlemail.com>
2024-11-19 16:10:45 +0100

    drafted some code needed to introduce units to scale configuration

--- src/yaw/cosmology.py
+++ src/yaw/cosmology.py
@@ -17,6 +17,8 @@ import numpy as np
 from astropy.cosmology import FLRW, Planck15, cosmology_equal
 from astropy.units import Quantity
 
+from yaw.options import Unit
+
 if TYPE_CHECKING:
     from numpy.typing import ArrayLike, NDArray
 
@@ -87,6 +89,74 @@ def cosmology_is_equal(cosmo1: TypeCosmology, cosmo2: TypeCosmology) -> bool:
 
 def get_default_cosmology() -> FLRW:
     return Planck15
+
+
+class Scales:
+    def __init__(
+        self,
+        scale_min: ArrayLike,
+        scale_max: ArrayLike,
+        *,
+        unit: Unit = Unit.kpc,
+        cosmology: TypeCosmology | None = None,
+    ) -> None:
+        self.unit = Unit(unit)
+        self.cosmology = cosmology or get_default_cosmology()
+
+        scale_min: NDArray = np.atleast_1d(scale_min)
+        scale_max: NDArray = np.atleast_1d(scale_max)
+
+        if scale_min.ndim != scale_max.ndim and scale_min.ndim != 1:
+            raise ValueError(  # TODO: ConfigError
+                "'scale_min/max' must be scalars or one-dimensional arrays"
+            )
+        if len(scale_min) != len(scale_max):
+            # TODO: ConfigError
+            raise ValueError("number of elements in 'scale_min/max' does not match")
+        if np.any((scale_max - scale_min) <= 0.0):
+            raise ValueError(  # TODO: ConfigError
+                "'scale_min' must be smaller than corresponding 'scale_max'"
+            )
+
+        self.scale_min = scale_min
+        self.scale_max = scale_max
+
+    def get_angle(self, redshift: ArrayLike) -> tuple[NDArray, NDArray]:
+        if self.unit in (Unit.deg, Unit.arcmin, Unit.arcsec):
+            deg_min = self.scale_min
+            deg_max = self.scale_max
+            if self.unit == Unit.arcmin:
+                deg_min = deg_min / 60.0
+                deg_max = deg_min / 60.0
+            elif self.unit == Unit.arcsec:
+                deg_min = deg_min / 3600.0
+                deg_max = deg_min / 3600.0
+            return np.deg2rad(deg_min), np.deg2rad(deg_max)
+
+        elif self.unit in (Unit.kpc, Unit.Mpc):
+            mpc_min = self.scale_min
+            mpc_max = self.scale_max
+            if self.unit == Unit.kpc:
+                mpc_min = mpc_min / 1000.0
+                mpc_max = mpc_max / 1000.0
+
+            rad_per_mpc = 1.0 / self.cosmology.angular_diameter_distance(redshift)
+            if isinstance(rad_per_mpc, Quantity):
+                rad_per_mpc = rad_per_mpc.value
+
+            return rad_per_mpc * mpc_min, rad_per_mpc * mpc_max
+
+        elif self.unit in (Unit.kpc_h, Unit.Mpc_h):
+            mpc_min = self.scale_min
+            mpc_max = self.scale_max
+            if self.unit == Unit.kpc_h:
+                mpc_min = mpc_min / 1000.0
+                mpc_max = mpc_max / 1000.0
+
+            comoving_dist = self.cosmology.comoving_distance(redshift)
+            return mpc_min / comoving_dist, mpc_max / comoving_dist
+
+        return self.scale_min, self.scale_max  # Unit.rad
 
 
 def separation_physical_to_angle(
