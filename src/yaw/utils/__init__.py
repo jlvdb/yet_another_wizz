c35c122f981ef918597cfd018ff9bf179cc17791
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-11-20 14:45:53 +0000

    renamed and rewrote the get_logger function

--- src/yaw/utils/__init__.py
+++ src/yaw/utils/__init__.py
@@ -3,7 +3,7 @@ Implements some utility functions for parallel processing, base classes, or
 logging that are used in various places in `yet_another_wizz`.
 """
 
-from yaw.utils.logging import get_default_logger
+from yaw.utils.logging import get_logger
 from yaw.utils.misc import (
     HDF_COMPRESSION,
     common_len_assert,
@@ -22,7 +22,7 @@ __all__ = [
     "format_float_fixed_width",
     "format_long_num",
     "format_time",
-    "get_default_logger",
+    "get_logger",
     "groupby",
     "is_legacy_dataset",
     "load_version_tag",
