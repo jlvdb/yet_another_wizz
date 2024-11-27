44e64686f947c47dcc943fffcdc2c2d318175a3f
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-11-27 20:33:02 +0000

    do not use ANSI colors when not supported

--- src/yaw/cli/utils.py
+++ src/yaw/cli/utils.py
@@ -2,14 +2,23 @@ from __future__ import annotations
 
 import sys
 
+from yaw.utils.logging import term_supports_color
+
+SUPPORTS_COLOR = term_supports_color()
+
 
 def print_message(msg: str, *, colored: bool, bold: bool) -> None:
     """Print a message that matches the YAW pretty logging style."""
-    color_code = 34 if colored else 37
-    style_code = 1 if bold else 0
-    color = f"\033[{style_code};{color_code}m"
+    if SUPPORTS_COLOR:
+        color_code = 34 if colored else 37
+        style_code = 1 if bold else 0
+        color = f"\033[{style_code};{color_code}m"
+        reset = "\033[0m"
+    else:
+        color = ""
+        reset = ""
 
     prefix = "CLI | "
-    message = f"{color}{prefix}{msg}\033[0m\n"
+    message = f"{color}{prefix}{msg}{reset}\n"
     sys.stdout.write(message)
     sys.stdout.flush()
