6e9b3da60ebc87725d883b83bd488f319a33d0b4
Jan Luca van den Busch <jlvdb@astro.rub.de>
2024-11-28 12:59:24 +0000

    updated commandline parser

--- src/yaw/cli/commandline.py
+++ src/yaw/cli/commandline.py
@@ -30,96 +30,108 @@ def path_exists(path: str) -> Path:
 
 
 class NameSpace:
-    verbose: int
-    progress: bool
+    # required
     wdir: Path
     setup: Path
-    config_from: Path | None
+    # optional
     cache_path: Path | None
+    workers: int | None
     drop: bool
     overwrite: bool
-    threads: int | None
+    resume: bool
+    verbose: int
+    progress: bool
 
     @classmethod
     def create_parser(cls) -> ArgumentParser:
         parser = argparse.ArgumentParser(
             formatter_class=argparse.RawDescriptionHelpFormatter,
-            description="yet_another_wizz: modular clustering redshift pipeline.\n\n"
-            "Batch processing tasks from configuration file.",
-            epilog="Thank you for using yet_another_wizz. Please consider citing "
-            "'A&A 642, A200 (2020)' when publishing results obtained from this code.",
-        )
-        parser.add_argument(
-            "--version", action="version", version=f"yet_another_wizz v{__version__}"
-        )
-        parser.add_argument(
-            "-v",
-            "--verbose",
-            action="count",
-            default=0,
-            help="show info-level log messages on terminal, repeat to show "
-            "debug-level messages",
-        )
-        parser.add_argument(
-            "--progress",
-            action="store_true",
-            help="show a progress bar for long-running tasks",
+            description=(
+                "modular clustering redshift pipeline for yet_another_wizz.\n"
+                "\n"
+                "Batch processing tasks from a YAML configuration file."
+            ),
+            epilog=(
+                "Thank you for using yet_another_wizz.\n"
+                "Please cite 'A&A 642, A200 (2020)' in published work."
+            ),
+            add_help=False,
         )
 
         parser.add_argument(
             "wdir",
             metavar="<path>",
             type=path_absolute,
-            help="project directory, must not exist",
+            help="project directory to use for this run",
+        )
+        parser.add_argument(
+            "setup",
+            type=path_exists,
+            metavar="<file>",
+            help="setup YAML file with configuration, input files, and task list",
         )
 
-        group_setup = parser.add_argument_group(
+        config_group = parser.add_argument_group(
             title="setup configuration",
-            description="select a setup file to run with optional modifcations",
+            description="configure parameter overrides, overwriting outputs, logging",
         )
-        group_setup.add_argument(
+        config_group.add_argument(
+            "--cache-path",
+            metavar="<path>",
+            type=path_absolute,
+            help="override cache path from setup file (inputs.cachepath)",
+        )
+        config_group.add_argument(
+            "--workers",
+            type=int,
+            metavar="<int>",
+            help="override number of parallel workers from setup file (correlation.max_workers)",
+        )
+        config_group.add_argument(
+            "--drop",
+            action="store_true",
+            help="drop cached catalogs after successful completion",
+        )
+        config_group.add_argument(
+            "--overwrite",
+            action="store_true",
+            help="overwrite project directory if it exists",
+        )
+        config_group.add_argument(
+            "--resume",
+            action="store_true",
+            help="resume operations on a previously interrupted run",
+        )
+        config_group.add_argument(
+            "-v",
+            "--verbose",
+            action="count",
+            default=0,
+            help="show info-level log messages on terminal, repeat argument for debug-level",
+        )
+        config_group.add_argument(
+            "--progress",
+            action="store_true",
+            help="show a progress bar for long-running tasks",
+        )
+
+        help_group = parser.add_argument_group(
+            title="help and information",
+            description="documentation resources",
+        )
+        help_group.add_argument(
+            "-h", "--help", action="help", help="show this help message and exit"
+        )
+        help_group.add_argument(
             "-d",
             "--dump",
             action=DumpConfigAction,
             const="default",
             nargs=0,
-            help="dump an empty setup file with default values to the terminal",
+            help="dump an empty configuration file with default values to the terminal",
         )
-        group_setup.add_argument(
-            "-s",
-            "--setup",
-            required=True,
-            type=path_exists,
-            metavar="<file>",
-            help="setup YAML file with configuration, input files and task list",
-        )
-        group_setup.add_argument(
-            "--config-from",
-            type=path_exists,
-            metavar="<file>",
-            help="load the 'configuration' section from this setup file",
-        )
-        group_setup.add_argument(
-            "--cache-path",
-            metavar="<path>",
-            type=path_absolute,
-            help="replace the 'data.cachepath' value in the setup file",
-        )
-        group_setup.add_argument(
-            "--drop",
-            action="store_true",
-            help="automatically drop the cache after successful completion",
-        )
-        group_setup.add_argument(
-            "--overwrite",
-            action="store_true",
-            help="automatically overwrite an existing project",
-        )
-        group_setup.add_argument(
-            "--threads",
-            type=int,
-            metavar="<int>",
-            help="number of threads to use (default: from configuration)",
+        help_group.add_argument(
+            "--version", action="version", version=f"yet_another_wizz v{__version__}"
         )
 
         return parser
