1d4ec9a2f3a5281ad213b76b4d662060e55a02dd
Jan Luca van den Busch <jlvdb@astro.rub.de>
2025-01-15 13:24:34 +0000

    attempt to create temporary test directories in /dev/shm if possible

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,13 @@ from __future__ import annotations
 
 import os
 
+from pytest import fixture
+
 # disable mulitprocessing, which is only beneficial on large datasets
 os.environ["YAW_NUM_THREADS"] = "1"
+
+
+@fixture
+def tmpdir_factory_with_shm(tmpdir_factory):
+    shm_path = "/dev/shm" if os.path.exists("/dev/shm") else "/tmp"
+    return tmpdir_factory.mktemp("pytest", dir=shm_path)
